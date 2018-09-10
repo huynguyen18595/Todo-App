084fa5a5ddcd2e44922a9feb923e424fabd5afc7
Steve Nguyen <huynguyen18595@gmail.com>
2018-09-11 04:51:39 +1000

    make seed, create modal, create controller for todo

---
+++ app/Http/Controllers/TodosController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+class TodosController extends Controller
+{
+    //
+    public function index(){
+      $todo = Todo::all();
+      return view('todos')->with('todos',$todo);
+    }
+}
--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -78,16 +78,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    This is the new file that've just been created 
+                    Todos
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div>
+                  @foreach($todos as $todo)
+                    {{$todo -> todo}}
+                  <hr>
+                  @endforeach
                 </div>
             </div>
         </div>
